73e830f06d1c6ac8cee8edaaeacdedaf649262cf
Annette Maza <nettymaza@gmail.com>
2017-09-22 22:30:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,9 +51,9 @@ class Student
 
   def self.new_from_db(row)
     id = row[0]
-    Student.new(id, name, grade)
     name = row[1]
     grade = row[2]
+    Student.new(id, name, grade)
   end
 
   def self.find_by_name(name)
